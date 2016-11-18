61e871a907c7412123334835539ff2a0f1c1bfa3
Adam Perry <adam.n.perry@gmail.com>
2016-11-18 15:32:03 -0700

    better lighting, collision detection

--- 2DGame/index.js
+++ 2DGame/index.js
@@ -1,49 +1,38 @@
 import Exponent from 'exponent';
 import React from 'react';
-import { Alert, Dimensions, PanResponder } from 'react-native';
+import { Alert, Dimensions, PanResponder, Text, View } from 'react-native';
 
-// Can't use `import ...` form because THREE uses oldskool module stuff.
 const THREE = require('three');
-
-// `THREEView` wraps a `GLView` and creates a THREE renderer that uses
-// that `GLView`. The class needs to be constructed with a factory so that
-// the `THREE` module can be injected without exponent-sdk depending on the
-// `'three'` npm package.
 const THREEView = Exponent.createTHREEViewClass(THREE);
 
 import Assets from '../Assets';
 
-
-//// Game
-
-// Render the game as a `View` component.
-
 export default (viewProps) => {
+  // constants
+  const BALL_RADIUS = 40;
+  const PLAYER_WH = 75;
+  const PLAYER_L = 125;
   const { width, height } = Dimensions.get('window');
 
-  const camera = new THREE.PerspectiveCamera(80, 1, 1, 10000);
-  camera.position.z = 1000;
+  // game state
+  let playerHealth = 100;
+  let distanceTraveled = 0;
+  let goodBalls = new Array();
+  let timeSinceGoodBall = 0;
+  let timeUntilGoodBall = 0.3;
+  let timeElapsed = 0;
 
 
   //// Scene, sprites
 
   // We just use a regular `THREE.Scene`
   const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(80, 1, 1, 10000);
+  camera.position.z = 1000;
 
-  // Making a sprite involves three steps which are outlined below. You probably
-  // would want to combine them into a utility function with defaults pertinent
-  // to your game.
+  const playerGeometry = new THREE.BoxGeometry(PLAYER_WH, PLAYER_WH, PLAYER_L);
 
-  // 1: Geometry
-  // This defines the local shape of the object. In this case the geometry
-  // will simply be a 1x1 plane facing the camera.
-  const playerGeometry = new THREE.BoxGeometry(200, 200, 200);
-
-  // 2: Material
-  // This defines how the surface of the shape is painted. In this case we
-  // want to paint a texture loaded from an asset and also tint it.
-  // Nearest-neighbor filtering with `THREE.NearestFilter` is nice for
-  // pixel art styles.
+  // TODO make a better texture & mesh for player
   const texture = THREEView.textureFromAsset(Assets['player-sprite']);
   texture.minFilter = texture.magFilter = THREE.NearestFilter;
   texture.needsUpdate = true;
@@ -53,51 +42,49 @@ export default (viewProps) => {
     transparent: true,  // Use the image's alpha channel for alpha.
   });
 
-
-
-  // 3: Mesh
-  // A mesh is a node in THREE's scenegraph and refers to a geometry and a
-  // material to draw itself. It can be translated and rotated as any other
-  // scenegraph node.
-
-  const tunnelGeometry = new THREE.CylinderGeometry(500, 500, 2000, 32, 1, true);
-  const tunnelMaterial = new THREE.MeshLambertMaterial({color: 0x00fff0});
-  const tunnelMesh = new THREE.Mesh(tunnelGeometry, tunnelMaterial);
-
-  tunnelMesh.rotation.x = Math.PI / 2;
-
-  tunnelMesh.flipSided = true;
-  scene.add(tunnelMesh);
-
-  // Geometries and materials can be reused.
   const playerMesh = new THREE.Mesh(playerGeometry, playerMaterial);
   playerMesh.position.z = 50;
   playerMesh.rotation.z = Math.PI;
   scene.add(playerMesh);
 
   const playerLight = new THREE.SpotLight(0xffffff);
-  playerLight.power = 8;
-  playerLight.penumbra = 0.4;
-  playerLight.decay = 2;
+  playerLight.power = 10000;
+  playerLight.penumbra = 0.7;
+  playerLight.decay = 1;
   playerLight.position.z = 50;
   playerLight.castShadow = true;
-  playerLight.shadow.mapSize.width = 1024;
-  playerLight.shadow.mapSize.height = 1024;
-  playerLight.shadow.camera.near = 500;
+  playerLight.shadow.mapSize.width = 512;
+  playerLight.shadow.mapSize.height = 512;
+  playerLight.shadow.camera.near = 100;
   playerLight.shadow.camera.far = 4000;
   playerLight.shadow.camera.fov = 70;
   scene.add(playerLight);
 
-  let goodBalls = new Array();
-  const badBalls = new Array();
-  let timeSinceGoodBall = 0;
-  let timeUntilGoodBall = 0.3;
-  let timeElapsed = 0;
+  const playerLightTarget= new THREE.Object3D();
+  playerLightTarget.position.z = -1000;
+  playerLight.target = playerLightTarget;
+  scene.add(playerLightTarget);
 
-  const goodMaterial = new THREE.MeshLambertMaterial({color: 0xcc0000});
-  const sphereGeometry = new THREE.SphereGeometry(20, 32, 32);
+  const goodMaterial = new THREE.MeshStandardMaterial({color: 0xaa0000});
+  const sphereGeometry = new THREE.SphereGeometry(BALL_RADIUS, 32, 32);
 
   //// Events
+
+  const restart = () => {
+    for (let ball of goodBalls) {
+      scene.remove(ball);
+    }
+
+    playerHealth = 100;
+    distanceTraveled = 0;
+    goodBalls = new Array();
+    timeSinceGoodBall = 0;
+    timeUntilGoodBall = 0.3;
+    timeElapsed = 0;
+
+    playerMesh.position.x = 0;
+    playerMesh.position.y = 0;
+  }
 
   // This function is called every frame, with `dt` being the time in seconds
   // elapsed since the last call.
@@ -106,53 +93,72 @@ export default (viewProps) => {
     timeSinceGoodBall += dt;
 
     if (timeUntilGoodBall >= 0.001) {
-      timeUntilGoodBall -= 0.0003;
+      timeUntilGoodBall -= 0.00035;
     }
 
     // add a new good ball at a random position
     if (timeSinceGoodBall > timeUntilGoodBall) {
       let newGoodBall = new THREE.Mesh(sphereGeometry, goodMaterial);
-      newGoodBall.position.x = ((Math.random() * width * 3) - ((width * 3) / 2));
-      newGoodBall.position.y = ((Math.random() * height * 3) - ((height * 3) / 2));
+      newGoodBall.position.x = ((Math.random() * width * 5) - ((width * 5) / 2));
+      newGoodBall.position.y = ((Math.random() * height * 5) - ((height * 5) / 2));
       newGoodBall.position.z = -500;
       goodBalls.push(newGoodBall);
       scene.add(newGoodBall);
       timeSinceGoodBall = 0;
     }
 
-    const zTick = 15;
+    const zTick = 400 * dt;
+    distanceTraveled += zTick / 1000;
+
     const ballsLeft = new Array();
     for (let goodBall of goodBalls) {
       goodBall.position.z += zTick;
 
       if (goodBall.position.z <= (camera.position.z - 400)) {
-        ballsLeft.push(goodBall);
+
+        // detect collision with player
+        let distance = goodBall.position.distanceTo(playerMesh.position);
+        if (distance <= PLAYER_WH + BALL_RADIUS) {
+          // TODO show sparks
+          playerHealth -= 300 / distance;
+          console.log('player health', playerHealth);
+          scene.remove(goodBall);
+        } else {
+          // only keep the ball if it doesn't collide
+          ballsLeft.push(goodBall);
+        }
       } else {
         scene.remove(goodBall);
       }
     }
+
     goodBalls = ballsLeft;
+
+    // TODO change player color based on health
+
+
+    // if player health <= 0, display loss and restart game
+    if (playerHealth <= 0) {
+      restart();
+    }
   }
 
   // These functions are called on touch and release of the view respectively.
   const touch = (_, gesture) => {
+    // move the player and light to the touched location
     const newX = (gesture.x0 - (width / 2)) * 5;
     const newY = -(gesture.y0 - (height/2)) * 5;
     playerMesh.position.x = newX;
     playerMesh.position.y = newY;
     playerLight.position.x = newX;
     playerLight.position.y = newY;
+    playerLightTarget.position.x = newX;
+    playerLightTarget.position.y = newY;
   };
   const release = (_, gesture) => {
     return;
   }
 
-
-  //// React component
-
-  // We bind our `touch` and `release` callbacks using a `PanResponder`. The
-  // `THREEView` takes our `scene` and `camera` and renders them every frame.
-  // It also takes our `tick` callbacks and calls it every frame.
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: touch,
